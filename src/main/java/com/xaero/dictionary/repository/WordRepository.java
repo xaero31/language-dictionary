58f35ade6e8149f1d5e806789f29b5bca3878da6
xaero <stub>
2022-02-04 22:47:25 +0300

    add random select for words repository

--- src/main/java/com/xaero/dictionary/repository/WordRepository.java
+++ src/main/java/com/xaero/dictionary/repository/WordRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WordRepository extends CrudRepository<WordEntity, Long> {
 
     @Query(value = "select * from words limit 1 offset abs(random()) % (select count(*) from words)", nativeQuery = true)
     WordEntity findRandom();
+
+    @Query(value = "select * from words order by random() limit :count", nativeQuery = true)
+    List<WordEntity> findRandomWords(int count);
 }
